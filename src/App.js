cdfd52d1d930bc5be5453382f436c307b473f534
jkdearnley <jkdearnley@gmail.com>
2019-08-24 18:16:55 +1200

    SignUp/SignIn pages added (Not linked to buttons)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import NavBar from './components/layout/navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import DiscussionDetails from './components/discussions/DiscussionDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+
 
 const App = () => {
   return (
@@ -13,6 +16,8 @@ const App = () => {
                 <Switch>
                   <Route exact path='/' component={Dashboard} />
                   <Route exact path='/discussion/:id' component={DiscussionDetails} />
+                  <Route exact path='/signin' component={SignIn} />
+                  <Route exact path='/signup' component={SignUp} />
                 </Switch> 
               </div>
           </BrowserRouter>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = () => {
     return(
         <ul className="right">
-            <li><NavLink to='/' className='btn blue darken-1'>Log In</NavLink></li>
+            <li><NavLink to='/' className='btn blue darken-1'>Sign In</NavLink></li>
             <li><NavLink to='/'>Sign Up</NavLink></li>
            
         </ul>
