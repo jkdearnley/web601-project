1c042759154a643dea56cac6b5d4a29d034f3614
jkdearnley <jkdearnley@gmail.com>
2019-08-24 15:45:06 +1200

    Signin/up etc. added to navbar

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
     return(
         <ul className="right">
-            <li><NavLink to='/' className='btn green lighten-1'>Start Discussion</NavLink></li>
+            <li><NavLink to='/' className='btn blue darken-1'>Start Discussion</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
-            <li><NavLink to='/' className='btn btn-floating green lighten-1'></NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating green darken-2'>JD</NavLink></li>
         </ul>
     )
 }
---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const SignedOutLinks = () => {
+    return(
+        <ul className="right">
+            <li><NavLink to='/' className='btn blue darken-1'>Log In</NavLink></li>
+            <li><NavLink to='/'>Sign Up</NavLink></li>
+           
+        </ul>
+    )
+}
+
+export default SignedOutLinks
