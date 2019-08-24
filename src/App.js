c2b12ab2363e3e0f559787a7abbd5aca23ee6d48
jkdearnley <jkdearnley@gmail.com>
2019-08-24 16:53:20 +1200

    Home button route added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import NavBar from './components/layout/navbar'
+import Dashboard from './components/dashboard/Dashboard'
+
 
 const App = () => {
   return (
       <div className="ui container">
           <BrowserRouter>
               <div>
-              <NavBar />
-                  
+                <NavBar />
+                <Switch>
+                  <Route path='/' component={Dashboard} />
+                </Switch> 
               </div>
           </BrowserRouter>
       </div>
---
+++ src/components/discussions/DiscussionList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const DiscussionList = () => {
+    return (
+        <div className="discussion-list section">
+
+          <div className="card z-depth-0 discussion-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Discussion Title</span>
+                  <p>Posted by Jayden</p>
+                  <p className="grey-text">24th August, 4:20pm</p>
+              </div>
+          </div>
+
+          <div className="card z-depth-0 discussion-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Discussion Title</span>
+                  <p>Posted by Jayden</p>
+                  <p className="grey-text">24th August, 4:20pm</p>
+              </div>
+          </div>
+
+          <div className="card z-depth-0 discussion-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Discussion Title</span>
+                  <p>Posted by Jayden</p>
+                  <p className="grey-text">24th August, 4:20pm</p>
+              </div>
+          </div>
+
+        </div>
+    )
+}
+
+export default DiscussionList
