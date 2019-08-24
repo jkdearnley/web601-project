1b9307499916e4b1119fcbecb55fce1ea1db1900
jkdearnley <jkdearnley@gmail.com>
2019-08-24 15:31:33 +1200

    Buttons added to navbar

--- src/components/layout/SigndInLinks.js
+++ src/components/layout/SigndInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
     return(
         <ul className="right">
-            <li><NavLink to='/'>Start Discussion</NavLink></li>
+            <li><NavLink to='/' className='btn green lighten-1'>Start Discussion</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating green lighten-1'></NavLink></li>
         </ul>
